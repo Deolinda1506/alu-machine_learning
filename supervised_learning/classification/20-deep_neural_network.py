50db4350d1247794671d78ad5761d5acd7a7ba01
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2025-05-18 21:56:23 +0000

    task 20

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -1,9 +1,10 @@
 
 #!/usr/bin/env python3
 """
-    A class DeepNeuralNetwork that defines a deep neural
-    network performing binary classification
+A class DeepNeuralNetwork that defines a deep neural
+network performing binary classification.
 """
+
 
 import numpy as np
 
