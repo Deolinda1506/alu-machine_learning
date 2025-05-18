dbd16564eb113644ffb33d23e0765ee378e44e5f
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2025-05-18 20:32:46 +0000

    Updated

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -1,77 +1,68 @@
-
 #!/usr/bin/env python3
-"""This module defines a single neuron for binary classification."""
-
+"""Defines a single neuron performing binary classification"""
 import numpy as np
 
 
 class Neuron:
-    """
-        Initialize a Neuron performing binary classification.
-
-        Args:
-            nx (int): The number of input features to the neuron.
-        Raises:
-            TypeError: If nx is not an integer.
-            ValueError: If nx is less than 1.
-        """
+    """Neuron class for binary classification"""
 
     def __init__(self, nx):
+        """
+        Initialize the neuron
+        Args:
+            nx (int): number of input features
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
         if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
+            raise TypeError("nx must be a integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
+            raise ValueError("nx must be positive")
+
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
     @property
     def W(self):
-        '''Getter for Weights'''
+        """Getter for weights vector"""
         return self.__W
 
     @property
     def b(self):
-        '''Getter for Bias'''
+        """Getter for bias"""
         return self.__b
 
     @property
     def A(self):
-        '''Getter for Activation function'''
+        """Getter for activated output"""
         return self.__A
 
     def forward_prop(self, X):
         """
-        Calculates the forward propagation of the neuron.
+        Calculates the forward propagation of the neuron
 
         Args:
-            X (numpy.ndarray): Input data of shape (nx, m), where
-            nx is the number of input features
-            and m is the number of examples.
-
+            X (ndarray): shape (nx, m) with input data
         Returns:
-            numpy.ndarray: The activated output of the neuron (A).
+            The activated output A (numpy.ndarray)
         """
-        # Linear combination Z = W.X + b
         Z = np.dot(self.__W, X) + self.__b
-
-        # Sigmoid activation function A = 1 / (1 + e^(-Z))
         self.__A = 1 / (1 + np.exp(-Z))
-
         return self.__A
 
     def cost(self, Y, A):
         """
-            Calculates the cost of the model using logistic regression.
+        Calculates the cost using logistic regression
 
-            Args:
-                Y (numpy.ndarray): Correct labels (1, m) for the input data.
-                A (numpy.ndarray): Activated output (1, m) for each example.
+        Args:
+            Y (ndarray): shape (1, m) with correct labels
+            A (ndarray): shape (1, m) with activated outputs
 
-            Returns:
-                float: The cost (cross-entropy loss).
-            """
+        Returns:
+            cost (float): logistic regression cost
+        """
         m = Y.shape[1]
-        cost = - (1 / m) * np.sum(Y * np.log(A) +
-                                  (1 - Y) * np.log(1.0000001 - A))
+        cost = - (1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
